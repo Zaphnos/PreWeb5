e2d2e561ed10de12153306fb89f7876285e092ee
Azkh <91705993+Zaphnos@users.noreply.github.com>
2023-12-08 14:06:38 +0700

    Add files via upload

---
+++ PreWeb5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database = "preprak";
+    public $port = 3306;
+}
